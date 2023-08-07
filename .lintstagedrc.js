1c726f210c7ee1518bf71cd1593752ee4d63c930
Oktora Kevin Arigi <oktora.kevin.arigi@gmail.com>
2023-08-07 15:41:47 +0700

    update latest version next

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,9 @@
-const path = require('path')
- 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`
+const path = require("path");
+
+const buildEslintCommand = filenames =>
+  `next lint --fix --file ${filenames.map(f => path.relative(process.cwd(), f)).join(" --file ")}`;
 
 module.exports = {
-  '*.{js,jsx,ts,tsx,json}': ['prettier --write'],
-  'src/**/*.(ts|tsx)': () => [buildEslintCommand, 'tsc --noEmit'],
-}
+  "*.{js,jsx,ts,tsx,json}": ["prettier --write", buildEslintCommand],
+  "src/**/*.(ts|tsx)": () => ["tsc --noEmit"],
+};
