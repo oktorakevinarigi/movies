6d4a4b41aa06e58f9ce35aa82dcb00bcf8434d0e
Oktora Kevin Arigi <oktora.kevin.arigi@gmail.com>
2023-08-09 23:50:45 +0700

    add unit test

--- src/components/feature/movies/stories/card.stories.tsx
+++ src/components/feature/movies/stories/card.stories.tsx
@@ -1,3 +1,5 @@
+import { screen } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 import { ULR_IMAGE } from "@/constants";
 import { Card } from "../card";
 import type { Meta, StoryObj } from "@storybook/react";
@@ -23,5 +25,9 @@ export const Desktop: CardMoviesStory = {
     title: "The Godfather",
     urlImage: ULR_IMAGE + "rSPw7tgCH9c6NqICZef4kZjFOQ5.jpg",
     year: "1972",
+  },
+  play: async () => {
+    const titleElement = await screen.findByText("The Godfather");
+    expect(titleElement).toBeInTheDocument();
   },
 };
